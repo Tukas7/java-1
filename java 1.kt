6ba62a492a944d59842f858afee53ded16707df6
Tukas7 <vma2302@bk.ru>
2023-01-17 20:44:49 +0500

    FirstCommit

---
+++ java 1.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Вискунов Максим, Студент")
+    val byte = 8
+    val short =16
+    val int = 32
+    val long =64
+    println ("$byte $short $int $long")
+    var smth=readln()
+    println("вы написали это:$smth")
+    println(smth[2])
+    val len= smth.length
+    println(len)
+
+}
